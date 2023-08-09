406d0415a5fdff5de5644827adfde31ac5724fa6
kerber <kerber@users.noreply.github.com>
2023-08-09 22:52:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="kerber"
+original_name="summary"
+original_urlname="summary"
+original_description="Awesome summary created by kerber"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv summary $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ summary/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for summary."""
+
+from summary.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
